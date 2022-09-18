e2ca3f1947e3c704368afe60bcc913edb9ce63db
Vitalii Khmurach <vitalii.khmurach@globallogic.com>
2022-09-18 12:53:43 +0300

    Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
+import { NotfoundComponent } from './notfound/notfound.component';
+import { BlankLayoutComponent } from './layouts/blank-layout/blank-layout.component';
+import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // app routes
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    loadChildren: () => import('./main/main.module').then((m) => m.MainModule),
+  },
+  // auth routes
+  {
+    path: 'auth',
+    component: BlankLayoutComponent,
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  // 404
+  {
+    path: '**',
+    component: NotfoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
+import { BlankLayoutComponent } from './layouts/blank-layout/blank-layout.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultLayoutComponent,
+    BlankLayoutComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
